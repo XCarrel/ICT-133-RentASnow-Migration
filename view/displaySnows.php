0f359d4b83d829126b334d1318af5c5d708f6f1e
MisterX25 <Xavier.Carrel@cpnv.ch>
2020-03-26 13:10:24 +0100

    Show list in a flexbox

--- view/displaySnows.php
+++ view/displaySnows.php
@@ -11,15 +11,15 @@ $title = "RentASnow - Snows";
 ?>
 <div class="row-fluid">
     <h1>Nos Snowboards :</h1>
-    <?php foreach ($snows as $snow) {?>
-        <div class="case_snow">
-            <img src="view/images/Snows/<?= $snow['photo'] ?>" class="listimages" alt="">
-            <h2>Marque : <?= $snow['brand'] ?></h2>
-            <h2>Model : <?= $snow['model'] ?></h2>
-            <a href="index.php?action=displaySnowDetails&id=<?= $snow['id'] ?>" style="text-decoration: underline;color: #4DB9EE">Voir Plus</a>
-        </div>
-        <hr>
-    <?php } ?>
+    <div class="snows">
+        <?php foreach ($snows as $snow) { ?>
+            <div class="snow">
+                <img src="view/images/Snows/<?= $snow['photo'] ?>" class="listimages" alt=""><br>
+                <a href="index.php?action=displaySnowDetails&id=<?= $snow['id'] ?>" style="text-decoration: underline;color: #4DB9EE"><?= $snow['brand'] ?> <?= $snow['model'] ?></a>
+            </div>
+            <hr>
+        <?php } ?>
+    </div>
 </div>
 
 <?php
